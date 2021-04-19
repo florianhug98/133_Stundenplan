8379a3891bd7f68315eed8e9391cffd0c1eca6a0
FlorianHugCH <florian.hug@bs.ch>
2021-04-19 12:20:41 +0200

    populating of options + reseting when new option selected adjusted

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ $(document).ready(
     function(){
 
         let date
-        setup()
+        setup();
 
         /**
          * Alle Vorarbeiten für die Funktion des Skripts:
@@ -11,12 +11,12 @@ $(document).ready(
          *  - etc.
          */
         function setup(){
-            moment.locale("de")
-            date = moment()
+            moment.locale("de");
+            date = moment();
 
-            $("#classes-wrapper").hide()
-            resetWarnings()
-            loadJobs()
+            $("#classes-wrapper").hide();
+            reset();
+            loadJobs();
         }
 
         /**
@@ -27,17 +27,22 @@ $(document).ready(
                 .done(function (jobs){
                         if (jobs.length > 0){
                             $.each(jobs, function (key, value){
-                                if (localStorage.getItem("job") != null && value.beruf_id === localStorage.getItem("job")){
-                                    $("#jobs").append(new Option(value.beruf_name, value.beruf_id, true, true))
-                                    loadClasses(value.beruf_id)
-                                }else {
-                                    $("#jobs").append(new Option(value.beruf_name, value.beruf_id))
-                                }
+                                // if (localStorage.getItem("job") != null && value.beruf_id === localStorage.getItem("job")){
+                                //     $("#jobs").append(new Option(value.beruf_name, value.beruf_id, true, true))
+                                //     loadClasses(value.beruf_id)
+                                // }else {
+                                //     $("#jobs").append(new Option(value.beruf_name, value.beruf_id))
+                                // }
+                                $("#jobs").append(new Option(value.beruf_name, value.beruf_id))
                             })
+                            if (localStorage.getItem("job") != null){
+                                $("#jobs option[value=" + localStorage.getItem("job") + "]").prop("selected", true);
+                                loadClasses(localStorage.getItem("job"));
+                            }
                         }
                     })
                 .fail(function(){
-                createErrorDiv("Beim laden der Berufe ist ein Fehler aufgetreten", true)
+                createErrorDiv("Beim laden der Berufe ist ein Fehler aufgetreten", true);
             })
         }
 
@@ -45,26 +50,29 @@ $(document).ready(
          * Eventhandler bei Aushwahl eines Berufes aus dem Dropdown
          */
         $("#jobs").change(function (){
-            const selectedJob = $("#jobs").find(":selected").val();
-            localStorage.setItem("job", selectedJob)
-            loadClasses(selectedJob)
+            const selectedJob = $("#jobs").find(":selected").val()
+            localStorage.removeItem("class");
+            reset();
+
+            localStorage.setItem("job", selectedJob);
+            loadClasses(selectedJob);
         })
 
         /**
          * Eventhandler bei Auswahl einer Klasse aus dem Dropdown
          */
         $("#classes").change(function (){
-            const selectedClass = $("#classes").find(':selected').val()
-            localStorage.setItem("class", $("#classes").find(':selected').val())
-            loadTimetable(selectedClass)
+            const selectedClass = $("#classes").find(':selected').val();
+            localStorage.setItem("class", $("#classes").find(':selected').val());
+            loadTimetable(selectedClass);
         })
 
         /**
          * Eventhandler bei Klick auf den Button für die vorherige Woche
          */
         $("#previousWeek").click(function (){
-            date.subtract(1, "w")
-            updateWeekSelector()
+            date.subtract(1, "w");
+            updateWeekSelector();
         })
 
 
@@ -72,8 +80,8 @@ $(document).ready(
          * Eventhandler bei Klick auf den Button für die nächste Woche
          */
         $("#nextWeek").click(function (){
-            date.add(1, "w")
-            updateWeekSelector()
+            date.add(1, "w");
+            updateWeekSelector();
         })
 
         /**
@@ -89,27 +97,32 @@ $(document).ready(
          * @param jobId - ID des ausgewählten Berufs
          */
         function loadClasses(jobId){
-            resetWarnings()
+            reset();
             $("#classes").find('option').remove().end();
             $.getJSON("http://sandbox.gibm.ch/klassen.php?beruf_id=" + jobId)
                 .done(function (classes){
                     if (classes.length > 0){
                         $("#classes").append("<option selected disabled>-- Wähle eine Klasse aus --</option>")
                         $.each(classes, function (key, value){
-                            if (localStorage.getItem("class") != null && value.klasse_id === localStorage.getItem("class")){
-                                $("#classes").append(new Option(value.klasse_longname, value.klasse_id, true, true))
-                                loadTimetable(value.klasse_id)
-                            }else {
-                                $("#classes").append(new Option(value.klasse_longname, value.klasse_id))
-                            }
+                            // if (localStorage.getItem("class") != null && value.klasse_id === localStorage.getItem("class")){
+                            //     $("#classes").append(new Option(value.klasse_longname, value.klasse_id, true, true))
+                            //     loadTimetable(value.klasse_id)
+                            // }else {
+                            //     $("#classes").append(new Option(value.klasse_longname, value.klasse_id))
+                            // }
+                            $("#classes").append(new Option(value.klasse_longname, value.klasse_id));
                         })
-                        $("#classes-wrapper").fadeIn("fast")
+                        if (localStorage.getItem("class") != null){
+                            $("#classes option[value= " + localStorage.getItem("class") + "]").prop("selected", true);
+                            loadTimetable(localStorage.getItem("class"));
+                        }
+                        $("#classes-wrapper").fadeIn("fast");
                     }else{
-                        $("#classes-wrapper").hide()
-                        createErrorDiv("Für den gewählten Beruf sind keine Klassen vorhanden", false)
+                        $("#classes-wrapper").hide();
+                        createErrorDiv("Für den gewählten Beruf sind keine Klassen vorhanden", false);
                     }
                 }).fail(function () {
-                createErrorDiv("Beim laden der Berufe ist ein Fehler aufgetreten", true)
+                createErrorDiv("Beim laden der Berufe ist ein Fehler aufgetreten", true);
             })
         }
 
@@ -118,14 +131,14 @@ $(document).ready(
          * @param classId - ID der gewünschten Klasse
          */
         function loadTimetable(classId){
-            resetWarnings()
+            reset();
             $("#selectedWeek").text(date.week() + " - " + date.year());
-            $("#weekSelector").show()
-            $("#timetable tbody tr").remove().end()
+            $("#weekSelector").show();
+            $("#timetable tbody tr").remove().end();
             $.getJSON("http://sandbox.gibm.ch/tafel.php?klasse_id=" + classId + "&woche=" + date.week() + '-' + date.year())
                 .done(function (timetable){
                     if (timetable.length > 0){
-                        $("#timetable-wrapper").fadeIn("fast")
+                        $("#timetable-wrapper").fadeIn("fast");
                         $.each(timetable, function (key, value){
                             $("#timetable").append(
                                 "<tr>" +
@@ -137,13 +150,13 @@ $(document).ready(
                                 "<td>" + value.tafel_lehrer + "</td>" +
                                 "<td>" + value.tafel_raum + "</td>" +
                                 "</tr>")
-                        })
+                        });
                     }else{
-                        createErrorDiv("Für den Zeitraum ist kein Stundenplan vorhanden", false)
+                        createErrorDiv("Für den Zeitraum ist kein Stundenplan vorhanden", false);
                     }
                 })
                 .fail(function(){
-                    createErrorDiv("Beim laden des Stundenplanes ist ein Fehler aufgetreten", true)
+                    createErrorDiv("Beim laden des Stundenplanes ist ein Fehler aufgetreten", true);
                 })
         }
 
@@ -156,22 +169,22 @@ $(document).ready(
             if (fatal){
                 $("#error").append(
                     "<div id=\"errorContent\" class=\"alert alert-danger\" role=\"alert\">\n" + error + "</div>"
-                ).hide().fadeIn("fast")
+                ).hide().fadeIn("fast");
             }else {
                 $("#error").append(
                     "<div id=\"errorContent\" class=\"alert alert-warning\" role=\"alert\">\n" + error + "</div>"
-                ).hide().fadeIn("fast")
+                ).hide().fadeIn("fast");
             }
         }
 
         /**
          * Reseten aller Fehlermeldungen
          */
-        function resetWarnings(){
-            $("#timetable-wrapper").hide()
-            $("#weekSelector").hide()
+        function reset(){
+            $("#timetable-wrapper").hide();
+            $("#weekSelector").hide();
 
-            $("#errorContent").remove()
+            $("#errorContent").remove();
         }
     }
 )
